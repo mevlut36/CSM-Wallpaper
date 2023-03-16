07166522ce91b5a177092c55cd2047449d8f4dcd
Mevlut <36679013+mevlut36@users.noreply.github.com>
2023-03-17 00:12:49 +0100

    Add files via upload

---
+++ src/assets/tracks.js
@@ -0,0 +1,122 @@
+
+import Opening_1 from './mp3/OP-1.mp3';
+import Ending_1 from './mp3/Ending-1.mp3';
+import Ending_2 from './mp3/Ending-2.mp3';
+import Ending_3 from './mp3/Ending-3.mp3';
+import Ending_4 from './mp3/Ending-4.mp3';
+import Ending_5 from './mp3/Ending-5.mp3';
+import Ending_6 from './mp3/Ending-6.mp3';
+import Ending_7 from './mp3/Ending-7.mp3';
+import Ending_8 from './mp3/Ending-8.mp3';
+import Ending_9 from './mp3/Ending-9.mp3';
+import Ending_10 from './mp3/Ending-10.mp3';
+import Ending_11 from './mp3/Ending-11.mp3';
+import Ending_12 from './mp3/Ending-12.mp3';
+
+import Opening_1_Image from './images/op-1.png';
+import Ending_1_Image from './images/ending-1.png';
+import Ending_2_Image from './images/ending-2.png';
+import Ending_3_Image from './images/ending-3.png';
+import Ending_4_Image from './images/ending-4.png';
+import Ending_5_Image from './images/ending-5.png';
+import Ending_6_Image from './images/ending-6.png';
+import Ending_7_Image from './images/ending-7.png';
+import Ending_8_Image from './images/ending-8.png';
+import Ending_9_Image from './images/ending-9.png';
+import Ending_10_Image from './images/ending-10.png';
+import Ending_11_Image from './images/ending-11.png';
+import Ending_12_Image from './images/ending-12.png';
+
+export const tracks = [
+  {
+    title: '『KICK BACK』',
+    src: Opening_1,
+    author: 'Kenshi Yonezu',
+    thumbnail: Opening_1_Image,
+    color: '#b55050',
+  },
+  {
+    title: '『CHAINSAW BLOOD』',
+    src: Ending_1,
+    author: 'Vaundy',
+    thumbnail: Ending_1_Image,
+    color: '#484848',
+  },
+  {
+    title: '『Hawatari Niku Centi』',
+    src: Ending_2,
+    author: 'Maximum the Hormone',
+    thumbnail: Ending_2_Image,
+    color: '#082e38',
+  },
+  {
+    title: '『Time Left』',
+    src: Ending_3,
+    author: 'ZUTOMAYO',
+    thumbnail: Ending_3_Image,
+    color: '#3b5754',
+  },
+  {
+    title: '『Tablet』',
+    src: Ending_4,
+    author: 'TOOBOE',
+    thumbnail: Ending_4_Image,
+    color: '#c70e0f',
+  },
+  {
+    title: '『In the backroom』',
+    src: Ending_5,
+    author: 'Syudou',
+    thumbnail: Ending_5_Image,
+    color: '#916931',
+  },
+  {
+    title: '『Rendezvous』',
+    src: Ending_6,
+    author: 'Kanaria',
+    thumbnail: Ending_6_Image,
+    color: '#969696',
+  },
+  {
+    title: '『Chu, Tayousei』',
+    src: Ending_7,
+    author: 'Ano',
+    thumbnail: Ending_7_Image,
+    color: '#0d2b52',
+  },
+  {
+    title: '『First death』',
+    src: Ending_8,
+    author: 'TK',
+    thumbnail: Ending_8_Image,
+    color: '#76c5da',
+  },
+  {
+    title: '『Deep down』',
+    src: Ending_9,
+    author: 'Aimer',
+    thumbnail: Ending_9_Image,
+    color: '#666766',
+  },
+  {
+    title: '『Dogland』',
+    src: Ending_10,
+    author: 'People 1',
+    thumbnail: Ending_10_Image,
+    color: '#372e21',
+  },
+  {
+    title: '『VIOLENCE』',
+    src: Ending_11,
+    author: 'Queen Bee',
+    thumbnail: Ending_11_Image,
+    color: '#444444',
+  },
+  {
+    title: '『Fight song』',
+    src: Ending_12,
+    author: 'Eve',
+    thumbnail: Ending_12_Image,
+    color: '#a0938a',
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AudioPlayer from './components/AudioPlayer';
+
+import './styles/index.css';
+import './styles/customize-progress-bar.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AudioPlayer />
+  </React.StrictMode>
+);
